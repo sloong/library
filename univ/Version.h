12d37d2a8f1c130bbcbb33207ef52a4e0b4f10db
Wang Chuanbin <wcb@sloong.com>
2016-02-17 14:48:51 +0800

    update version

--- univ/Version.h
+++ univ/Version.h
@@ -3,9 +3,9 @@
 
 
 // Version
-#define VERSION_NUMBER						2,0,0,77
-#define VERSION_FILEVERSION					L"2.0.0.77"
-#define VERSION_BUILDTIME					L"2014/10/29"
+#define VERSION_NUMBER						2,0,1,109
+#define VERSION_FILEVERSION					L"2.0.1.109"
+#define VERSION_BUILDTIME					L"2016/02/17"
 #ifdef _DEBUG
 #define VERSION_PRODUCTVERSION 				L"Ver.2.0 for Debug"
 #define	VERSION_FILEDESCRIPTION				L"Sloong Universal Debug Libaray"
@@ -17,7 +17,7 @@
 #endif // _DEBUG
 #define VERSION_INTERNALNAME				L"Universal.dll")
 #define VERSION_COMPANYNAME 				L"Sloong, Inc.")
-#define	VERSION_LEGALCOPYRIGHT				L"Copyright (C) 2014-2015 Sloong, Inc."
+#define	VERSION_LEGALCOPYRIGHT				L"Copyright (C) 2014-2016 Sloong, Inc."
 
 
 #endif // !VERSION_H

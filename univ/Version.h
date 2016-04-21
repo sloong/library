cbbb81b4ac38b0f5dd4f647366be0e5c094027e9
Wang Chuanbin <wcb@sloong.com>
2016-04-21 11:09:41 +0800

    add auto verison

--- univ/Version.h
+++ univ/Version.h
@@ -3,9 +3,9 @@
 
 
 // Version
-#define VERSION_NUMBER						2,1,1,130
-#define VERSION_FILEVERSION					L"2.1.1.130"
-#define VERSION_BUILDTIME					L"2016/04/01"
+#define VERSION_NUMBER						2,1,1,131
+#define VERSION_FILEVERSION					L"2.1.1.131"
+#define VERSION_BUILDTIME					L"$CUR_DATE"
 #ifdef _DEBUG
 #define VERSION_PRODUCTVERSION 				L"Ver.2.1 for Debug"
 #define	VERSION_FILEDESCRIPTION				L"Sloong Universal Debug Libaray"
--- version.sh
+++ version.sh
@@ -1,1 +1,9 @@
-git rev-list --all | wc -l
+#!/bin/bash
+rm -f univ/version.h
+_GIT_VERSION=`git rev-list --all | wc -l`
+
+cat version.h.template | sed "s/\$GIT_VERSION/$_GIT_VERSION/g" > univ/version.h
+
+echo "Generated version.h"
+
+git add univ/version.h

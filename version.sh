f219c19792098921c3b0699b0d8723cbc9d6038a
Wang Chuanbin <wcb@sloong.com>
2016-04-21 11:23:41 +0800

    add ver

--- version.sh
+++ version.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-rm -f univ/version.h
+VER_PATH=univ/version.h
+rm -f $VER_PATH
 _GIT_VERSION=`git rev-list --all | wc -l`
-
-cat version.h.template | sed "s/\$GIT_VERSION/$_GIT_VERSION/g" > univ/version.h
-
-echo "Generated version.h"
-
-git add univ/version.h
+cat version.h.template | sed "s/\$GIT_VERSION/$_GIT_VERSION/g" > $VER_PATH
+ echo "Generated version.h"
+git add $VER_PATH

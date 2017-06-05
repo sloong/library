4f479ea47a4cd15b150e5be49528cb94d3503451
wcb <wcb@sloong.com>
2017-06-05 15:00:46 +0800

    modify:modify some error.

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ cp sloong.conf /etc/ld.so.conf.d/sloong.conf
 mkdir -p /usr/local/include/univ
 mkdir -p /usr/local/lib/sloong
 cp -f include/univ/*.h /usr/local/include/univ/
-cp -f $(PROGRAM) /usr/local/lib/sloong/
+cp -f libuniv.so /usr/local/lib/sloong/
--- univ/version.h
+++ univ/version.h
@@ -3,8 +3,8 @@
 
 
 // Version
-#define VERSION_NUMBER						2,2,6,170
-#define VERSION_FILEVERSION					L"2.2.6.170"
+#define VERSION_NUMBER						2,2,6,172
+#define VERSION_FILEVERSION					L"2.2.6.172"
 #define VERSION_BUILDTIME					L"2016/07/27"
 
 #ifdef _DEBUG

36e5ce77c02c7cee7c346765a5aee59029563ff0
wcb <wcb@sloong.com>
2017-06-28 20:47:25 +0800

    update version.

--- univ/version.h
+++ univ/version.h
@@ -3,16 +3,16 @@
 
 
 // Version
-#define VERSION_NUMBER						2,2,6,176
-#define VERSION_FILEVERSION					L"2.2.6.176"
-#define VERSION_BUILDTIME					L"2016/07/27"
+#define VERSION_NUMBER						2,3,0,181
+#define VERSION_FILEVERSION					L"2.3.0.181"
+#define VERSION_BUILDTIME					L"2017/06/28"
 
 #ifdef _DEBUG
-#define VERSION_PRODUCTVERSION 				L"Ver.2.2 for Debug"
+#define VERSION_PRODUCTVERSION 				L"Ver.2.3 for Debug"
 #define	VERSION_FILEDESCRIPTION				L"Sloong Universal Debug Libaray"
 #define	VERSION_PRODUCTNAME					L"Sloong Universal Debug Libaray"
 #else
-#define VERSION_PRODUCTVERSION 				L"Ver.2.2")
+#define VERSION_PRODUCTVERSION 				L"Ver.2.3")
 #define	VERSION_FILEDESCRIPTION				L"Sloong Universal Libaray"
 #define	VERSION_PRODUCTNAME					L"Sloong Universal Libaray"
 #endif // _DEBUG

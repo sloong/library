759ed9151f4469029dde917a33be79543d018224
wcb <wcb@sloong.com>
2018-02-11 15:12:28 +0800

    update to v2.4.0

--- univ/version.h
+++ univ/version.h
@@ -3,22 +3,22 @@
 
 
 // Version
-#define VERSION_NUMBER						2,3,1,201
-#define VERSION_FILEVERSION					L"2.3.1.201"
-#define VERSION_BUILDTIME					L"2017/06/28"
+#define VERSION_NUMBER						2,4,0,202
+#define VERSION_FILEVERSION					L"2.4.0.202"
+#define VERSION_BUILDTIME					L"2018/02/11"
 
 #ifdef _DEBUG
-#define VERSION_PRODUCTVERSION 				L"Ver.2.3 for Debug"
+#define VERSION_PRODUCTVERSION 				L"Ver.2.4 for Debug"
 #define	VERSION_FILEDESCRIPTION				L"Sloong Universal Debug Libaray"
 #define	VERSION_PRODUCTNAME					L"Sloong Universal Debug Libaray"
 #else
-#define VERSION_PRODUCTVERSION 				L"Ver.2.3")
+#define VERSION_PRODUCTVERSION 				L"Ver.2.4")
 #define	VERSION_FILEDESCRIPTION				L"Sloong Universal Libaray"
 #define	VERSION_PRODUCTNAME					L"Sloong Universal Libaray"
 #endif // _DEBUG
 #define VERSION_INTERNALNAME				L"Universal.dll")
 #define VERSION_COMPANYNAME 				L"Sloong, Inc.")
-#define	VERSION_LEGALCOPYRIGHT				L"Copyright (C) 2014-2017 Sloong, Inc."
+#define	VERSION_LEGALCOPYRIGHT				L"Copyright (C) 2014-2018 Sloong, Inc."
 
 
 #endif // !VERSION_H
